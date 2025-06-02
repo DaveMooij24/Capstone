6fc3afef98a3dee4608261b3f981d326e59d39df
Dave Mooij <dave.mooij@hva.nl>
2025-06-02 21:26:48 +0200

    fixed loading issue

--- app/src/main/java/nl/hva/capstone/ui/components/LoadingOverlay.kt
+++ app/src/main/java/nl/hva/capstone/ui/components/LoadingOverlay.kt
@@ -1,22 +1,34 @@
 package nl.hva.capstone.ui.components
 
-import androidx.compose.foundation.*
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.*
-import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 
 @Composable
 fun LoadingOverlay(isLoading: Boolean) {
     if (isLoading) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color(0xFF40D6C0).copy(alpha = 0.5f)),
-            contentAlignment = Alignment.Center
+        Dialog(
+            onDismissRequest = {}, 
+            properties = DialogProperties(
+                usePlatformDefaultWidth = false,
+                decorFitsSystemWindows = false
+            )
         ) {
-            CircularProgressIndicator(color = Color(0xFF40D6C0))
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0xFF40D6C0).copy(alpha = 0.5f)),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator(color = Color.White)
+            }
         }
     }
 }
+
+
